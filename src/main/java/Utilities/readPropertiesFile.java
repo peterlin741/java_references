8b82c884225270dcdf28c33c6c3c3739d8a01873
Peter Lin <peterlin741@gmail.com>
2017-04-23 19:31:31 -0400

    updating readPropertiesFile.java

--- src/main/java/Utilities/readPropertiesFile.java
+++ src/main/java/Utilities/readPropertiesFile.java
@@ -1,7 +1,31 @@
 package Utilities;
 
-/**
- * Created by peterlin on 4/23/17.
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class readPropertiesFile {
+    public static void main(String[] args) {
+        try {
+            Properties properties = new Properties();
+
+            File file = new File("src/main/resources/config.properties");
+            FileInputStream stream = new FileInputStream(file);
+
+            properties.load(stream);
+            stream.close();
+
+            String name = properties.getProperty("my.name", "no valid name given");
+            System.out.println("name = " + name);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
